a5d01c37f9085da4b75aa0bad8d760bfddb1a1ff
Teyah Patterson <pattersonteyah@gmail.com>
2024-05-12 02:35:07 -0500

    array exercises

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,25 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+console.log(str.split());
+console.log(str.split(' '));
+console.log(str.split(''));
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+arr.join('_');
+console.log(arr.join(','));
+console.log(arr.join(''));
 //3) Do split or join change the original string/array?
-
+console.log(str);
+console.log(arr);
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+let cargoArr = [];
+cargoArr = cargoHold.split(',');
+console.log(cargoArr);
+// alphabetizing cargoArr array
+cargoArr.sort();
+console.log(cargoArr);
+// making array back into list
+let finalCargoArr = '';
+finalCargoArr= cargoArr.join(',');
+console.log(finalCargoArr);
--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,12 @@
 //Create an array called practiceFile with the following entry: 273.15
-
+let practiceFile = [237.15];
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
-
+// adding 42 to array
+practiceFile = [237.15, 42];
+console.log(practiceFile);
+// adding hello to array
+practiceFile = [237.15, 42, "hello"];
+console.log(practiceFile);
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+practiceFile.push("87", false, -4.6,);
+console.log(practiceFile);
